2721af177faf81cc02deec17c483a1d3d58a45e7
Awanmandal <awanankit@gmail.com>
2026-03-20 21:49:14 +0530

    enter_in_file

---
+++ python/code1.py
@@ -0,0 +1,7 @@
+#Take two dynamic integers;
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+#now do their sum and keep in an another variable;
+sum=a+b
+#now print the sum; 
+print("The sum of the two numbers is: ",sum)
