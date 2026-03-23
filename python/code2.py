7328510928ed584557e6cf439a8abee64f604579
Awanmandal <awanankit@gmail.com>
2026-03-23 23:09:49 +0530

    Update code2.py

--- python/code2.py
+++ python/code2.py
@@ -2,7 +2,7 @@
 a=int(input("Enter the first number: "))
 b=int(input("Please enter another number: "))
 #do their product
-product=a*b
+produc=a*b
 #now print the product
-print("The product of the numbers: ",product)
+print("The product of the numbers: ",produc)
 
