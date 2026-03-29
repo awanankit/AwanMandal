686948274232b4825abd07398e79c79395d82f60
Awanmandal <awanankit@gmail.com>
2026-03-29 11:38:48 +0530

    project basic

--- python/code22.py
+++ python/code22.py
@@ -3,10 +3,10 @@ t = turtle.Turtle()
 t.speed(0)
 
 colors = ["red","blue","green","yellow","purple","orange"]
-
+ 
 for i in range(100):
     t.pencolor(colors[i % 6])
     t.circle(80)
-    t.left(5)
+    t.left(5) 
 
 turtle.done()
