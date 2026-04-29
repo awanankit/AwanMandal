0644994ab34f70b31850edd42244434145eb8913
Awanmandal <awanankit@gmail.com>
2026-04-29 20:38:47 +0530

    code

---
+++ python/voice.py
@@ -0,0 +1,13 @@
+import b as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Speak something...")
+    audio = r.listen(source)
+
+try:
+    text = r.recognize_google(audio)
+    print("You said:", text)
+except:
+    print("Sorry, could not understand")
